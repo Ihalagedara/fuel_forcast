917fe78e9c9dc53c518ff339239d44e7666bd616
IhalagedaraSB <ihalagedara.shamil@gmail.com>
2022-05-04 10:35:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from typing import List
 import databases
+import sqlalchemy
 from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+from pydantic import BaseModel
 import os
 import urllib
 
@@ -15,6 +19,98 @@ DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_p
 
 database = databases.Database(DATABASE_URL)
 
+metadata = sqlalchemy.MetaData()
+
+fuel = sqlalchemy.Table(
+    "fuel_forcast",
+    metadata,
+    sqlalchemy.Column("Site_ID", sqlalchemy.String, primary_key=True),
+    sqlalchemy.Column("Site_Name", sqlalchemy.String),
+    sqlalchemy.Column("Region", sqlalchemy.String),
+    sqlalchemy.Column("New_Region", sqlalchemy.String),
+    sqlalchemy.Column("Depot", sqlalchemy.String),
+    sqlalchemy.Column("Tower_Category", sqlalchemy.String),
+    sqlalchemy.Column("Energy_category", sqlalchemy.String),
+    sqlalchemy.Column("Entity", sqlalchemy.String),
+    sqlalchemy.Column("Site_category", sqlalchemy.String),
+    sqlalchemy.Column("Rectification_Rank", sqlalchemy.String),
+    sqlalchemy.Column("New_Rectification_Rank", sqlalchemy.String),
+    sqlalchemy.Column("Site_Cabin_Type", sqlalchemy.String),
+    sqlalchemy.Column("STBG_FTG_Status", sqlalchemy.String),
+    sqlalchemy.Column("Gen_brand_1", sqlalchemy.String),
+    sqlalchemy.Column("Gen_Capacity_1", sqlalchemy.String),
+    sqlalchemy.Column("Gen_Brand_2", sqlalchemy.String),
+    sqlalchemy.Column("Gen_Capacity_2", sqlalchemy.String),
+    sqlalchemy.Column("Fuel_Tank_Capacity", sqlalchemy.String),
+    sqlalchemy.Column("FuelConsumption_Fuel_consumption", sqlalchemy.String),
+    sqlalchemy.Column("FuelFilled1_Date", sqlalchemy.String),
+    sqlalchemy.Column("FuelFilled1_Filled_Fuel_Qty_L", sqlalchemy.String),
+    sqlalchemy.Column("RunningHr_TotalGenRunning", sqlalchemy.String),
+    sqlalchemy.Column("RegionaUpdate_STBG_FTG_Status", sqlalchemy.String),
+    sqlalchemy.Column("Remaining_Fuel_Quantity", sqlalchemy.String),
+    sqlalchemy.Column("Required_Fuel_Amount_for_Next_5_Day", sqlalchemy.String),
+    sqlalchemy.Column("Next_Filling_Date", sqlalchemy.String),
+    sqlalchemy.Column("Remark", sqlalchemy.String),
+    sqlalchemy.Column("RegionaUpdate_STBG_FTG_Status2", sqlalchemy.String),
+
+)
+
+engine = sqlalchemy.create_engine(
+    #DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, pool_size=3, max_overflow=0
+)
+metadata.create_all(engine)
+
+
+class Fuel(BaseModel):
+    Site_ID: str
+    Site_Name: str
+    Region: str
+    New_Region: str
+    Depot: str
+    Tower_Category: str 
+    Energy_category: str
+    Entity: str
+    Site_category: str
+    Rectification_Rank: str
+    New_Rectification_Rank: str
+    Site_Cabin_Type: str
+    STBG_FTG_Status: str
+    Gen_brand_1: str
+    Gen_Capacity_1: str
+    Gen_Brand_2: str
+    Gen_Capacity_2: str
+    Fuel_Tank_Capacity: str
+    FuelConsumption_Fuel_consumption: str
+    FuelFilled1_Date: str
+    FuelFilled1_Filled_Fuel_Qty_L: str
+    RunningHr_TotalGenRunning: str
+    RegionaUpdate_STBG_FTG_Status: str
+    Remaining_Fuel_Quantity: str
+    Required_Fuel_Amount_for_Next_5_Day: str
+    Next_Filling_Date: str
+    Remark: str
+    RegionaUpdate_STBG_FTG_Status2: str
+
+app = FastAPI(title="REST API using FastAPI PostgreSQL Async EndPoints")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+app.add_middleware(GZipMiddleware)
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 
 app = FastAPI()
@@ -25,11 +121,11 @@ async def first():
     return {"Hello" : "Test"}
 
 
-@app.get("/filter_less_30")
-async def filter_fuel_less_30():
-    
+@app.get("/filter_less_30",response_model=List[Fuel], status_code = status.HTTP_200_OK)
+async def filter_fuel_less_30(skip: int = 0, take: int = 20):
+    query = fuel.select().offset(skip).limit(take)
 
-    return {"Hello" : "Test1"}
+    return await database.fetch_all(query)
 
 @app.get("/filter_less_100")
 async def filter_fuel_less_100():
