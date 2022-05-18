fa16673bb6e986af22b20afb32afa408ff8d78a9
IhalagedaraSB <ihalagedara.shamil@gmail.com>
2022-05-18 12:04:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ driver= '{ODBC Driver 17 for SQL Server}'
 
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP (2000) * FROM [dbo].[Fuel-Forecast-CNR]")
+        cursor.execute("SELECT TOP (1500) * FROM [dbo].[Fuel-Forecast-CNR]")
         row = cursor.fetchall()
         #print(row[0][23])
 
